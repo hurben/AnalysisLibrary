03b546b645727605366da91f8a262ab5a228d05d
hurben <hurben.bioinfo@gmail.com>
2019-05-13 13:16:02 +0900

    adding_tex_comparison

--- function_library.py
+++ function_library.py
@@ -1,49 +1,68 @@
-###################### 19.05.12
+#------------------------------------------------- 19.05.12
 #
 #Objective : Practicing PEP and libraries that I am not good.
 #
 #
-#
+#------------------------------------------------------------
 
 
 class FileHandling:
 
-    def read_file(self, input_text, option):
+	def file_to_dataframe(self, input_data, option):
 
-        if option == "dataframe":
-            import pandas
+		import pandas
 
-            print "[Loading pandas]"
-            input_data = pandas.read_csv(input_text, sep=" ", header=None)
-            print "Loaded Dataframe : "
-            print input_data
-            print "Loading row 2 : "
-            print input_data[1:2]
-            print "Loading col 3 : "
-            print input_data[2]
+		print "> Loading pandas"
+		input_data = pandas.read_csv(input_text, sep=option, header=None)
+#		print "2 : "
+#		print input_data[1:2]
+#		print input_data['A']
+#		print input_data[input_data.columns[0]]
+		
+		return input_data
 
+	def file_to_readlines(self, input_data):
+	#> General Usage
+	#Load files that is not formed as a dataframe
 
+		open_file = open(input_data,'r')
+		readlines_file = open_file.readlines()
 
+		return readlines_file
 
+class DataHandling:
+	
+	def tokenize_list(self, input_list, option):
+		
+		input_list.replace('\n','') #
+		input_list.replace('\r','') #Replacing dirty formats that could caused by various type of inputs..
+		tokenized_list = input_list.split(option)
+		
+		
+		return tokenized_list
 
-
-
-
-
-
-
-
-
-
+		
 
 
 if __name__ == "__main__":
 
-    simple_text_input = './test_data/simple_test.txt'
-    data_type = "dataframe"
+	import sys
+	import argparse
 
-    FileHandling().read_file(simple_text_input, data_type)
+	parser = argparse.ArgumentParser()
 
+	parser.add_arguement("-i", "--input", dest="input_file")
+	parser.add_arguement("-s", "--sep", dest="sep")
+
+	args = parser.parse_args()
+	input_file = args.input_file
+	input_sep = args.sep
+	
+
+	simple_text_input = './test_data/simple_test.txt'
+
+	FileHandling().read_file(input_file, sep_type) 
 
 else:
-    print "Loading as FunctionLibrary"
+	print "Loading <function_library.py>"
+
